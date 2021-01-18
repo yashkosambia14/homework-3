e97bbfebceb5c3ae8080435e6fa8405a51ecb5ab
Hatimalhazmi <60146633+Hatimalhazmi@users.noreply.github.com>
2021-01-18 14:05:39 -0500

    Update and rename test_add_binary.py to test_numbers_and_strings.py

---
+++ tests/test_numbers_and_strings.py
@@ -0,0 +1,29 @@
+import pytest
+
+from mypkg.my_answers import add_binary
+
+def numbers_and_strings():
+    """
+    This is to review numbers and strings and basic operations.
+    """
+    # Write power of 2018
+    x = 2018 ** 2
+
+    # Assign a string "Stevens" to a variable y
+    y = "Stevens"
+
+    # Repeat variable y 5 times
+    z = y * 5
+
+    # What is the length of z?
+    length = len(z)
+
+    # Concatenate variable y with string " is good"
+    m = y + " is good"
+
+    # Replace "good" with "awesome" in variable m and assign it to a new variable n
+    n = m.replace("good", "awesome")
+
+    return x, y, z, length, m, n
+
+
