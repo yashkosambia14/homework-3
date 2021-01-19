e44171e5f3b96129e699d35a867df46deef3465d
Hatimalhazmi <60146633+Hatimalhazmi@users.noreply.github.com>
2021-01-19 08:56:04 -0500

    Update test_numbers_and_strings.py

--- tests/test_numbers_and_strings.py
+++ tests/test_numbers_and_strings.py
@@ -1,29 +1,31 @@
 import pytest
 
-from mypkg.my_answer import numbers_and_strings
+from mypkg.my_answers import numbers_and_strings
 
-def numbers_and_strings():
-    """
-    This is to review numbers and strings and basic operations.
-    """
-    # Write power of 2018
-    x = 2018 ** 2
+x, y, z, length, m, n = numbers_and_strings()
 
-    # Assign a string "Stevens" to a variable y
-    y = "Stevens"
 
-    # Repeat variable y 5 times
-    z = y * 5
+def test_power():
+    assert(x == 4072324)
 
-    # What is the length of z?
-    length = len(z)
 
-    # Concatenate variable y with string " is good"
-    m = y + " is good"
+def test_string():
+    assert(y == "Stevens")
 
-    # Replace "good" with "awesome" in variable m and assign it to a new variable n
-    n = m.replace("good", "awesome")
 
-    return x, y, z, length, m, n
+def test_repeat():
+    assert(len(z) == 5 * len("Stevens"))
+
+
+def test_len():
+    assert(length == 35)
+
+
+def test_concat():
+    assert(m == "Stevens is good")
+
+
+def test_replace():
+    assert(n == "Stevens is awesome")
 
 
