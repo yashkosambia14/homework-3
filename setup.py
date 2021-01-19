459a7cb76d80d9b560e501d6d18e6b4e0f080216
Hatimalhazmi <60146633+Hatimalhazmi@users.noreply.github.com>
2021-01-19 09:37:14 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import find_packages
 setup(name='homework1',
       version='1.0',
       description='Python Distribution Utilities',
-      author='Sergul Aydore',
+      author='Hatimalhazmi',
       packages=find_packages('src'),
       package_dir={'': 'src'},
       py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
